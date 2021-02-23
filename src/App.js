4b3c279e8613cc9c4921bcbece484f842d5a38d9
sidhantmathur <sidhant185@gmail.com>
2021-02-23 14:32:32 -0600

    remove cra html and css

--- src/App.js
+++ src/App.js
@@ -6,25 +6,11 @@ import Sidebar from './components/navbars/Sidebar'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <div className='layout'>
         <Sidebar />
         <Navbar />
       </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
