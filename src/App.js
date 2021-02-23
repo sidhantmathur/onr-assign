85f676f1c2536b9f649f4f3169aac869c05bae9d
sidhantmathur <sidhant185@gmail.com>
2021-02-23 14:27:28 -0600

    add sidebar component to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Navbar from './components/navbars/Navbar.js'
+import Navbar from './components/navbars/Navbar'
+import Sidebar from './components/navbars/Sidebar'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Sidebar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
