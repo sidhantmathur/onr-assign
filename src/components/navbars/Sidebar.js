a93b3c4fcfe5e3dcaa2d96d860bdf8e88ff113b2
sidhantmathur <sidhant185@gmail.com>
2021-02-23 14:25:31 -0600

    add sidebar component content

---
+++ src/components/navbars/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'Sidebar.css'
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div>Pin</div>
+      <div>Chart</div>
+      <div>Doc</div>
+      <div>Triangle</div>
+      <div>Doc</div>
+    </div>
+  )
+}
