d7f110cd61d5eea1821bd2e1a003055bc8558c07
sidhantmathur <sidhant185@gmail.com>
2021-02-23 14:08:52 -0600

    init navbar structure and import to app

---
+++ src/components/navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <div>Diagnostic Tool</div>
+      <ul>
+        <li>
+          Logged in as General User
+        </li>
+        <li>
+          |
+        </li>
+        <li>
+          |
+        </li>
+        <li>
+          |
+        </li>
+        <li>
+          |
+        </li>
+        <li>
+          |
+        </li>
+        <li>
+          |
+        </li>
+      </ul>
+    </div>
+  )
+}
