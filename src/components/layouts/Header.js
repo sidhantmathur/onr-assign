54c514ceb47c2a556e1994d4c520168c25620a5c
sidhantmathur <sidhant185@gmail.com>
2021-02-23 21:43:37 -0600

    add content and styling to second header

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Header.css'
 
 export default function Header() {
   return (
@@ -6,13 +7,25 @@ export default function Header() {
       <h1>Performance Management</h1>
       <h2>Diagnostic Tool</h2>
 
-      <div className="subHeaderFilters">
-        <div>
-
+      <div className="subheader">
+        <div className="filters">
+          <p>Filters</p>
+          <div className="filter-box">
+            <div><div className="blueBox"></div>All CQA Results</div>
+            <div><div className="greyBox"></div>CQAs with Closed Loop</div>
+          </div>
         </div>
 
-        <div>
-
+        <div className='quality-score'>
+          <div className='qs-title'>Quality Score Trend</div>
+          <div>
+            <button>Day</button>
+            <button>Week</button>
+            <button>Month</button>
+            <button>Quarter</button>
+            <button>Half</button>
+            <button>Year</button>
+          </div>
         </div>
       </div>
     </>
