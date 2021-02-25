daa801be8dd0cfdf901f68761e3163f77d9e730f
sidhantmathur <sidhant185@gmail.com>
2021-02-25 16:00:11 -0600

    start process of highlighting selected boxes

--- src/components/charts/PieContainer.js
+++ src/components/charts/PieContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 import PieChart from './PieChart'
 import './Pie.css'
 
-function PieContainer({ preData, setSelectedData }) {
+function PieContainer({ preData, setSelectedData}) {
   
   const arrayPre = [preData]
   const remainderSub = preData.score
@@ -18,11 +18,14 @@ function PieContainer({ preData, setSelectedData }) {
   
   const onButtonClick = (data) => {
     setSelectedData(data)
-    console.log(data);
+    // let selectedItem = document.getElementById(data)
+    // let unselectedItems = document.getElementsByClassName("gbox")
+    // unselectedItems.classList.remove("selected")
+    // selectedItem.classList.add("selected")
   }
 
   return (
-    <div className="gbox" onClick={() => onButtonClick(data[0].name)}>
+    <div id={data[0].name} className="gbox" onClick={() => onButtonClick(data[0].name)}>
       <p>{data[0].name}</p>
         <PieChart data={data} />
       <p style={{color: 'black'}}>Sample: {data[0].sample}</p>
